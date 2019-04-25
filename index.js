dbf0a3220215f86b517f4e378d7b502a852a105e
Yuval Greenfield <ubershmekel@gmail.com>
2019-04-25 14:32:58 -0700

    Randomize button

--- index.js
+++ index.js
@@ -26,6 +26,22 @@
         tiles: generateTiles(),
       },
       methods: {
+        randomizeTiles: function() {
+          this.tiles = generateTiles();
+        },
+        sortTiles: function() {
+          // Note, sorting colors seems to be something I know nothing about.
+          // I wonder what the correct function here is. Probably some sort of
+          // distance preserving curve like Hilbert.
+          this.tiles.sort((a, b) => {
+            a = a.hex;
+            b = b.hex;
+            var ai = a[1] + a[3] + a[5] + a[2] + a[4] + a[6];
+            var bi = b[1] + b[3] + b[5] + b[2] + b[4] + b[6];
+            return (ai).localeCompare(bi);
+            //return a.hex.localeCompare(b.hex);
+          });
+        }
       },
     });
   }
